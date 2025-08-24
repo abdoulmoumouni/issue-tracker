32716d3f7a4091a0cdacdfb26e7a6e654ae5a8be
abdoulmoumouni <anassougarba@gmail.com>
2025-08-24 13:09:53 +0100

    refactor issue creation page to use react-hook-form and SimpleMDE for description input and handle form submit

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,39 @@
-import { TextField, Flex, Button, Box, TextArea } from "@radix-ui/themes";
+"use client"
+import { useForm, Controller } from "react-hook-form"
 import React from "react";
+import { TextField, Flex, Button, Box, TextArea } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const IssueCreatePage = () => {
+  const {register,control,handleSubmit} = useForm<IssueForm>();
+  const router = useRouter()
   return (
-    <div className="max-w-xl p-5 space-y-3">
+    <form className="max-w-xl p-5 space-y-3" onSubmit={handleSubmit(async (data)=>{
+      await axios.post("/api/issues",data)
+      router.push("/issues")
+    } 
+    )}>
       <Flex direction="column" gap="3" maxWidth="100%">
-        <TextField.Root color="green" variant="soft" placeholder="Title"  />
+        <TextField.Root color="green" variant="soft" placeholder="Title" {...register("title")}  />
         <Box maxWidth="100%">
-		<TextArea size="3" placeholder="Reply to comment…"  rows={5} variant="soft" />
+          <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder="Reply to comment…" {...field}/>}
+          />
+		
 	</Box>
       </Flex>
         <Button variant="solid">Submit new issue</Button>
-    </div>
+    </form>
   );
 };
 
